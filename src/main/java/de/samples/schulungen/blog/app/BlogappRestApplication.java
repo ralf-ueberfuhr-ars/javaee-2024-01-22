88ceca25c5cd7b45b1efcb665a104744bc05d1e7
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2024-01-22 13:34:43 +0100

    first commit

---
+++ src/main/java/de/samples/schulungen/blog/app/BlogappRestApplication.java
@@ -0,0 +1,11 @@
+package de.samples.schulungen.blog.app;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class BlogappRestApplication extends Application {
+}

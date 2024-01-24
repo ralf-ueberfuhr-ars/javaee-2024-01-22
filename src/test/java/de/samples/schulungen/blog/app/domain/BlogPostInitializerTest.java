892c2560f09ce3bfb661d68a26c27da0386694a7
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2024-01-24 14:30:34 +0100

    Add test for initializer

--- src/test/java/de/samples/schulungen/blog/app/domain/BlogPostInitializerTest.java
+++ src/test/java/de/samples/schulungen/blog/app/domain/BlogPostInitializerTest.java
@@ -1,10 +1,7 @@
 package de.samples.schulungen.blog.app.domain;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
-import org.mockito.Mockito;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 

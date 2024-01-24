484536056d62ecb262a7919f81919f2240f5d07b
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2024-01-24 11:24:48 +0100

    Introduce Lambda sample

--- src/main/java/de/samples/schulungen/blog/app/boundary/ListAllBlogPostsServlet.java
+++ src/main/java/de/samples/schulungen/blog/app/boundary/ListAllBlogPostsServlet.java
@@ -11,7 +11,6 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.util.Collection;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
 @WebServlet("/list-blogposts")
